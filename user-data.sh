5bec5560fb0fe72e188c31126807617325b933ec
rknktm <erkanoktem@gmail.com>
2022-08-17 22:44:27 +0200

    project files

--- user-data.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-yum update -y
-yum install python3 -y
-pip3 install flask
-pip3 install flask_mysql
-yum install git -y
-cd /home/ec2-user 
-git clone https://${token}@github.com/rknktm/phonebook.git 
-python3 /home/ec2-user/phonebook/phonebook-app.py
